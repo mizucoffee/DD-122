debd7cd6b5d18f25b691f072e8f5d8f545a70e54
Ritsuki Goto <kawakawaritsuki.dev@gmail.com>
2017-02-28 02:40:54 +0900

    [add] timestamp

--- app/src/main/java/net/mizucoffee/hatsuyuki_chinachu/dashboard/recorded/RecordedCardRecyclerAdapter.java
+++ app/src/main/java/net/mizucoffee/hatsuyuki_chinachu/dashboard/recorded/RecordedCardRecyclerAdapter.java
@@ -1,7 +1,6 @@
 package net.mizucoffee.hatsuyuki_chinachu.dashboard.recorded;
 
 import android.content.Context;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,6 +12,7 @@ import android.widget.TextView;
 import net.mizucoffee.hatsuyuki_chinachu.R;
 import net.mizucoffee.hatsuyuki_chinachu.chinachu.api.model.gamma.Recorded;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -40,8 +40,12 @@ class RecordedCardRecyclerAdapter extends RecyclerView.Adapter<RecordedCardRecyc
 
     @Override
     public void onBindViewHolder(final ViewHolder vh, int position) {
-        vh.titleTv.setText(recorded.get(vh.getAdapterPosition()).getTitle());
-        vh.desTv.setText(recorded.get(vh.getAdapterPosition()).getDetail());
+        Recorded program = recorded.get(vh.getAdapterPosition());
+        vh.titleTv.setText(program.getTitle());
+        vh.desTv.setText(program.getDetail());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd E HH:mm");
+        SimpleDateFormat sdf1 = new SimpleDateFormat("HH:mm");
+        vh.timeTv.setText(sdf.format(program.getStart())+"-"+sdf1.format(program.getEnd()) + " " + (program.getSeconds() / 60) + "min");
     }
 
     @Override
@@ -53,8 +57,8 @@ class RecordedCardRecyclerAdapter extends RecyclerView.Adapter<RecordedCardRecyc
     class ViewHolder extends RecyclerView.ViewHolder {
         private ImageView imageView;
         private TextView  titleTv;
+        private TextView  timeTv;
         private TextView  desTv;
-        private CardView  card;
         private Button    playBtn;
         private Button    detailBtn;
         private Button    deleteBtn;
@@ -63,8 +67,8 @@ class RecordedCardRecyclerAdapter extends RecyclerView.Adapter<RecordedCardRecyc
             super(v);
             imageView  = ButterKnife.findById(v,R.id.image_view);
             titleTv    = ButterKnife.findById(v,R.id.title_tv);
+            timeTv     = ButterKnife.findById(v,R.id.time_tv);
             desTv      = ButterKnife.findById(v,R.id.des_tv);
-            card       = ButterKnife.findById(v,R.id.card_view);
             playBtn    = ButterKnife.findById(v,R.id.play_btn);
             detailBtn  = ButterKnife.findById(v,R.id.detail_btn);
             deleteBtn  = ButterKnife.findById(v,R.id.delete_btn);
